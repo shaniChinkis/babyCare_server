118b2ef1ac69cdb89c18d1ceccb7af8e50686612
shani ch <shanic0527@gmail.com>
2021-11-07 21:16:19 +0200

    Attempt to activate the signup function

--- controllers/signupGController.js
+++ controllers/signupGController.js
@@ -10,10 +10,10 @@ class SignUpG {
         return jwt.sign({ username }, TOKEN_SECRET);
     };
     signUpG = (req, res) => {
-        console.log("hello");
+        //console.log("hello");
 
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-        console.log(req.body.email);
+        console.log(req.body);
         const { user, password} = req.body; 
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
